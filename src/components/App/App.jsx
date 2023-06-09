a1d153b5ed1dba229c8c715c057b199cfb04d5f9
Adam Oberle <oberle.adam@gmail.com>
2023-06-08 21:21:40 -0500

    installed redux packages, sourced store, logger, and middleware into index.js, created store and wireframe for feedback reducer, sourced Router/route/link into app.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import axios from 'axios';
 import './App.css';
+import { HashRouter as Router, Route, Link } from 'react-router-dom';
+
 
 function App() {
 
@@ -10,6 +12,9 @@ function App() {
         <h1 className='App-title'>Feedback!</h1>
         <h4>Don't forget it!</h4>
       </header>
+      <Router>
+        
+      </Router>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,10 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App/App';
+import logger from 'redux-logger';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+
+
+const feedback = (state = [], action) => {
+    return state;
+}
+
+const storeInstance = createStore(
+    combineReducers({
+        feedback,
+    }),
+    applyMiddleware(logger),
+);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <App />
+        <Provider store={storeInstance}>
+            <App />
+        </Provider>
     </React.StrictMode>
 );
