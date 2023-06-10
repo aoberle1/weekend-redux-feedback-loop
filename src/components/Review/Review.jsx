acda7960401d4ba74e24c5a4f358f3c88b578b21
Adam Oberle <oberle.adam@gmail.com>
2023-06-09 19:47:23 -0500

    finished review page except submit, working on axios post

--- src/components/Review/Review.jsx
+++ src/components/Review/Review.jsx
@@ -3,10 +3,24 @@ import { useDispatch, useSelector } from "react-redux";
 import { useHistory } from "react-router-dom";
 
 function Review () {
-    const feeling = useSelector(store => store.feeling);
-    const understanding = useSelector(store => store.understanding);
-    const support = useSelector(store => store.support);
-    const comment = useSelector(store => store.comment);
+    // const feeling = useSelector(store => store.feeling);
+    // const understanding = useSelector(store => store.understanding);
+    // const support = useSelector(store => store.support);
+    // const comments = useSelector(store => store.comment);
+    const feedback = useSelector(store => store.feedback)
+
+    // const feedbackToSend = {
+    //     feeling,
+    //     understanding,
+    //     support,
+    //     comments
+    // }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log('feedback to send is:', feedback)
+
+    }
 
     return (
 
@@ -16,10 +30,23 @@ function Review () {
                 <tbody>
                     <tr>
                         <td>How you're feeling:</td>
-                        <td>{feeling}</td>
+                        <td>{feedback[0]}</td>
+                    </tr>
+                    <tr>
+                        <td>How well you understand the material:</td>
+                        <td>{feedback[1]}</td>
+                    </tr>
+                    <tr>
+                        <td>How supported you feel:</td>
+                        <td>{feedback[2]}</td>
+                    </tr>
+                    <tr>
+                        <td>Your Comments:</td>
+                        <td>{feedback[3]}</td>
                     </tr>
                 </tbody>
             </table>
+            <button onClick={handleSubmit}>Submit</button>
         </div>
 
     )
--- src/index.js
+++ src/index.js
@@ -6,45 +6,58 @@ import logger from 'redux-logger';
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 
-
-const feeling = (state = [], action) => {
+const feedback = (state = [], action) => {
     if (action.type === 'ADD_FEELING') {
         console.log(`The feeling value entered in store was: ${action.payload}`)
         return [...state, action.payload];
-    }
-    return state;
-}
-
-const understanding = (state = [], action) => {
-    if (action.type === 'ADD_UNDERSTANDING') {
+    } else if (action.type === 'ADD_UNDERSTANDING') {
         console.log(`The understanding value entered in store was: ${action.payload}`)
         return [...state, action.payload];
-    }
-    return state;
-}
-
-const support = (state = [], action) => {
-    if(action.type === 'ADD_SUPPORT'){
+    } else if(action.type === 'ADD_SUPPORT'){
         console.log(`The support value entered in store was: ${action.payload}`)
         return [...state, action.payload]
-    }
-    return state;
-}
-
-const comment = (state = [], action) => {
-    if(action.type === 'ADD_COMMENT') {
+    } else if(action.type === 'ADD_COMMENT') {
         console.log(`The comment entered in store was: ${action.payload}`)
         return [...state, action.payload]
     }
     return state;
 }
 
+// const feeling = (state = '', action) => {
+//     if (action.type === 'ADD_FEELING') {
+//         console.log(`The feeling value entered in store was: ${action.payload}`)
+//         return [...state, action.payload];
+//     }
+//     return state;
+// }
+
+// const understanding = (state = '', action) => {
+//     if (action.type === 'ADD_UNDERSTANDING') {
+//         console.log(`The understanding value entered in store was: ${action.payload}`)
+//         return [...state, action.payload];
+//     }
+//     return state;
+// }
+
+// const support = (state = '', action) => {
+//     if(action.type === 'ADD_SUPPORT'){
+//         console.log(`The support value entered in store was: ${action.payload}`)
+//         return [...state, action.payload]
+//     }
+//     return state;
+// }
+
+// const comment = (state = '', action) => {
+//     if(action.type === 'ADD_COMMENT') {
+//         console.log(`The comment entered in store was: ${action.payload}`)
+//         return [...state, action.payload]
+//     }
+//     return state;
+// }
+
 const storeInstance = createStore(
     combineReducers({
-        feeling,
-        understanding,
-        support,
-        comment
+        feedback
     }),
     applyMiddleware(logger),
 );
